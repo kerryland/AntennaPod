10ae8e8377f893166214244d3d489888885113a3
Kerry Sainsbury <kerry@fidelma.com>
2026-08-06 08:56:02 +1200

    Fixed crash when sort order was null

--- storage/database/src/main/java/de/danoeh/antennapod/storage/database/mapper/FeedItemJoinQuery.java
+++ storage/database/src/main/java/de/danoeh/antennapod/storage/database/mapper/FeedItemJoinQuery.java
@@ -1,5 +1,7 @@
 package de.danoeh.antennapod.storage.database.mapper;
 
+import static de.danoeh.antennapod.model.feed.SortOrder.PRIORITY_PLAYBACK_DATE_NEW_OLD;
+import static de.danoeh.antennapod.model.feed.SortOrder.PRIORITY_PLAYBACK_DATE_OLD_NEW;
 import static de.danoeh.antennapod.storage.database.PodDBAdapter.KEY_FEED;
 import static de.danoeh.antennapod.storage.database.PodDBAdapter.KEY_ID;
 import static de.danoeh.antennapod.storage.database.PodDBAdapter.TABLE_NAME_FEEDS;
@@ -9,13 +11,11 @@ import de.danoeh.antennapod.model.feed.SortOrder;
 
 public class FeedItemJoinQuery {
     public static String generateFrom(SortOrder sortOrder) {
-        switch (sortOrder) {
-            case PRIORITY_PLAYBACK_DATE_NEW_OLD:
-            case PRIORITY_PLAYBACK_DATE_OLD_NEW:
-                return  " JOIN " + TABLE_NAME_FEEDS + " ON " +
-                        TABLE_NAME_FEEDS + "." + KEY_ID + " = " + TABLE_NAME_FEED_ITEMS + "." + KEY_FEED;
-            default:
-                return "";
+        if (sortOrder == PRIORITY_PLAYBACK_DATE_NEW_OLD || sortOrder == PRIORITY_PLAYBACK_DATE_OLD_NEW) {
+            return " JOIN " + TABLE_NAME_FEEDS + " ON " +
+                    TABLE_NAME_FEEDS + "." + KEY_ID + " = " + TABLE_NAME_FEED_ITEMS + "." + KEY_FEED;
+
         }
+        return "";
     }
 }
